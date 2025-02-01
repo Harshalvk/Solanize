89629fd3ada3cd273d9a3196328a1f7da1f8c2cd
Harshalvk <harshalvkhobragade@gmail.com>
2025-02-01 14:19:57 +0530

    add: react-query

--- components/providers/AppProvider.tsx
+++ components/providers/AppProvider.tsx
@@ -1,16 +1,25 @@
-import { ReactNode } from "react";
+"use client";
+
+import { ReactNode, useState } from "react";
 import { ThemeProvider } from "./ThemeProvider";
 import WalletContextProvider from "@/app/(pages)/launchpad/_components/Wallet";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function AppProvider({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <ThemeProvider
-      attribute={"class"}
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <WalletContextProvider>{children}</WalletContextProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute={"class"}
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <WalletContextProvider>{children}</WalletContextProvider>
+        <ReactQueryDevtools />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }

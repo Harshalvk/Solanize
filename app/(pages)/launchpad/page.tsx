21b7ab27092d1271d65792c324af1f903a811c3c
Harshalvk <harshalvkhobragade@gmail.com>
2025-02-01 15:09:47 +0530

    fix: title

--- app/(pages)/launchpad/page.tsx
+++ app/(pages)/launchpad/page.tsx
@@ -13,8 +13,8 @@ const page = (props: Props) => {
   return (
     <div className="min-w-96 h-screen flex flex-col items-center justify-center">
       <div>
-        <h1 className="text-7xl font-semibold tracking-tighter text-center bg-gradient-to-t from-zinc-400 to-white bg-clip-text text-transparent mb-6">
-          Token Launchpad
+        <h1 className="text-5xl sm:text-6xl lg:text-7xl font-semibold tracking-tighter text-center bg-gradient-to-t from-zinc-400 to-white bg-clip-text text-transparent mb-6">
+          Solana Token Launchpad
         </h1>
       </div>
       <div className="w-2/3">

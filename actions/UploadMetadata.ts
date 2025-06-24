9035e6fa76c160dcee35c2d1156dc314d297aa75
Harshalvk <harshalvkhobragade@gmail.com>
2025-06-24 23:55:21 +0530

    refactor: update uploadMetadata function to use Metadata type and improve authorization handling

--- actions/UploadMetadata.ts
+++ actions/UploadMetadata.ts
@@ -1,13 +1,15 @@
-"use server"
+"use server";
 
-export async function uploadMetadata(metadata: any): Promise<string> {
+import { Metadata } from "@/lib/types";
+
+export async function uploadMetadata(metadata: Metadata): Promise<string> {
   const response = await fetch(
     "https://api.pinata.cloud/pinning/pinJSONToIPFS",
     {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer token`,
+        Authorization: `Bearer ${process.env.PINATA_JWT}`,
       },
       body: JSON.stringify(metadata),
     }

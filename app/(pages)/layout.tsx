d41df59b96dbd0cee493d747517a112630c26937
Harshalvk <harshalvkhobragade@gmail.com>
2025-06-24 23:55:14 +0530

    refactor: update Layout component structure for improved organization and layout

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,10 +1,15 @@
 import AppSidebar from "@/components/AppSidebar";
+import Navbar from "@/components/Navbar";
 import React from "react";
-export default function layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen">
-      <AppSidebar />
-      <div className="flex flex-col flex-1 min-h-screen">{children}</div>
+    <div className="flex flex-col h-screen w-full">
+      <Navbar />
+
+      <div className="flex flex-1 overflow-hidden">
+        <AppSidebar />
+        <div className="flex-1 m-4 rounded-md">{children}</div>
+      </div>
     </div>
   );
 }

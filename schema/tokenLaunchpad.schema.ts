0b566bde7d6286bdb43c93b23b9c4ab6370c33d6
Harshalvk <harshalvkhobragade@gmail.com>
2025-06-24 23:53:44 +0530

    fix: enhance validation messages for name and symbol fields in TokenLaunchpadFormSchema

--- schema/tokenLaunchpad.schema.ts
+++ schema/tokenLaunchpad.schema.ts
@@ -1,8 +1,16 @@
 import { z } from "zod";
 
 export const TokenLauchpadFormSchema = z.object({
-  name: z.string().min(3).max(16),
-  symbol: z.string(),
+  name: z
+    .string()
+    .min(3, {
+      message: "Token Name should be atleast 3 characters long.",
+    })
+    .max(16, { message: "Token Name should only contains 16 characters max." }),
+  symbol: z
+    .string()
+    .min(1, { message: "Token symbol must contain at least 1 character." })
+    .max(5, { message: "Token symbol should of 5 characters max" }),
   initial_supply: z.string(),
   image: z.string(),
   description: z.string(),
